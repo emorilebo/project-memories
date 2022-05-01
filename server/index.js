b84d354b8ad6ad4726ecb76472bf2a2d845ec55e
Godfrey Lebo <emorylebo@gmail.com>
2022-05-01 10:03:27 +0100

    Project server setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes); //localhost:5000/posts
+
+const CONNECTION_URL =
+  "mongodb+srv://emori:CRUXifixur1958@cluster0.vnubi.mongodb.net/memories?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//mongoose.set("useFindAndModify", false);
+
+//https://www.mongodb.com/cloud/atlas
